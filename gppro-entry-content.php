6d9524d493cdb5fd2a7acd09e81f79998ec18a68
Nick Croft <nick@copyblogger.com>
2018-04-20 17:21:36 -0400

    update copyright

--- gppro-entry-content.php
+++ gppro-entry-content.php
@@ -10,7 +10,7 @@ Author URI: https://reaktivstudios.com/
 */
 
 /*
-	Copyright 2014 Andrew Norcross, Reaktiv Studios
+	Copyright 2018 Reaktiv Studios, Reaktiv Studios
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
